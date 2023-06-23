e44867d0b585e11786707f5ef59e06fba8be9d3b
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-06-23 11:32:43 -0300

    Update script

--- snake.js
+++ snake.js
@@ -3,66 +3,109 @@ const context = canvas.getContext("2d");
 context.fillStyle = "#00FF00";
 const UNIT = 20;
 
-let position = {
-    x:0,
-    y:0
-};
-
-let direction = {
+const direction = {
     UP:"UP",
     LEFT:"LEFT",
     DOWN:"DOWN",
     RIGHT:"RIGHT",
 };
 
+let position = {
+    x:40,
+    y:0
+};
+
+let snake = [
+    [40, 0], [20, 0], [0, 0]
+];
+
 let way = direction.RIGHT;
-let fps = 0;
-let second = -1;
+let frame = 0;
+let skipframes = 10;
+let shouldMove = true;
+
+window.addEventListener("keydown", (e) => {
+    
+    if (e.code === "ArrowDown" && way != direction.UP) {
+        way = direction.DOWN
+    } else if (e.code === "ArrowLeft" && way != direction.RIGHT) {
+        way = direction.LEFT
+    } else if (e.code === "ArrowUp" && way != direction.DOWN) {
+        way = direction.UP
+    } else if (e.code === "ArrowRight" && way != direction.LEFT) {
+        way = direction.RIGHT
+    }
+  }, false
+);
 
 function loop() {
 
-    let nextSecond = new Date().getSeconds();
-    if (second != nextSecond) {
-        second = nextSecond;
-        console.log(fps);
-        fps = 1;
-    } else {
-        fps++;
+    frame++;
+    if (frame >= skipframes) {
+        frame = 0;
     }
 
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillRect(position.x, position.y, UNIT, UNIT);
+    if(!frame){
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        drawSnake();
+        
+        if(shouldMove){
 
-    switch (way) {
-
-        case direction.UP:
-            moveUp();
-            break;
-        case direction.RIGHT:
-            moveRight();
-            break;
-        case direction.DOWN:
-            moveDown();
-            break;
-        case direction.LEFT:
-            moveLeft();
-            break;
+            switch (way) {
+                case direction.UP:
+                    moveUp();
+                    break;
+                case direction.RIGHT:
+                    moveRight();
+                    break;
+                case direction.DOWN:
+                    moveDown();
+                    break;
+                case direction.LEFT:
+                    moveLeft();
+                    break;
+            }
+            updateSnake();
+            checkBounds();
+        } else {
+            resetSnake();
+            shouldMove=true;
+        }
     }
-    
-    checkBounds();
     requestAnimationFrame(loop);
 }
 
-function checkBounds(){
-    if(position.x + UNIT >= canvas.width && way == "RIGHT"){
-        way = direction.DOWN;
-    } else if(position.y + UNIT >= canvas.height && way == "DOWN"){
-        way = direction.LEFT;
-    } else if(position.x <= 0 && way == "LEFT"){
-        way = direction.UP;
-    } else if(position.y <= 0 && way == "UP"){
-        way = direction.RIGHT;
+function updateSnake(){
+    snake.pop();
+    snake.unshift([position.x, position.y]);
+}
+
+function drawSnake(){
+    for (let i = 0; i < snake.length; i++) {
+        context.fillRect(snake[i][0], snake[i][1], UNIT-2, UNIT-2);
+        console.log(snake[i]);
     }
+}
+
+function checkBounds(){
+    if(
+        (position.x + UNIT > canvas.width && way == direction.RIGHT) ||
+        (position.y + UNIT > canvas.height && way == direction.DOWN) ||
+        (position.x < 0 && way == direction.LEFT) ||
+        (position.y < 0 && way == direction.UP)) {
+        resetSnake();
+    }
+}
+
+function resetSnake(){
+    way = direction.RIGHT
+    position = {
+        x:40,
+        y:0
+    };
+    snake = [
+        [40, 0], [20, 0], [0, 0]
+    ];
 }
 
 function moveUp(){
@@ -81,4 +124,4 @@ function moveLeft(){
     position.x -= UNIT;
 }
 
-  requestAnimationFrame(loop);
+requestAnimationFrame(loop);
